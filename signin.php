ec3711988a2e971b6088920c3a6f91c4d0e53f94
CBNUKE <jimmy29922@gmail.com>
2014-04-23 21:18:13 +0700

    Change signin.php

--- signin.php
+++ signin.php
@@ -1,5 +1,16 @@
 <?php
 	include('include/connection.inc.php');
+
+  //Check login
+  if(isset($_POST['user'])&&$_POST['user']!=NULL&&isset($_POST['pass'])&&$_POST['pass']!=NULL){
+    $iuser = $_POST['user'];
+    $ipass = $_POST['pass'];
+    $sql = 'SELECT * FROM `member` WHERE `m_user` = \''.$iuser.'\' AND `m_pass` = \''.$ipass.'\'';
+    $query = mysqli_query($objConnect,$sql);
+    $flag = (mysqli_num_rows($query)==1)?TRUE:FALSE;
+    echo $flag;
+  }
+
 	include('include/header.inc.php');
 	showHeader('signin');
 ?>
@@ -11,12 +22,12 @@
     		<h3 class="panel-title"><strong>Please sign in</strong></h3>
   		</div>
   		<div class="panel-body">
- 			<form role="form">
+ 			<form role="form" method="post">
         		<div class="form-group">
-    			<input type="text" class="form-control" placeholder="Username" required autofocus>
+    			   <input name="user" type="text" class="form-control" placeholder="Username" required autofocus>
         		</div>
        		 	<div class="form-group">
-        		<input type="password" class="form-control" placeholder="Password" required>
+        		  <input name="pass" type="password" class="form-control" placeholder="Password" required>
         		</div>
         		<div class="form-group col-md-offset-2">
         		<button class="btn btn-primary col-md-4" style="margin-left:14px;" type="submit">Sign in</button>
